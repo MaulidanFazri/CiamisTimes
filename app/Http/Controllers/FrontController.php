edc968d8b6f25f38b7f2767e5434b969ad61a281
MaulidanFazri <maulidanfazri24@gmail.com>
2024-11-04 12:50:45 +0700

    Update FrontController and index view to display featured articles and banner advertisements

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -6,6 +6,7 @@ use App\Models\Author;
 use App\Models\Category;
 use App\Models\ArticleNews;
 use Illuminate\Http\Request;
+use App\Models\BannerAdvertisement;
 
 class FrontController extends Controller
 {
@@ -14,13 +15,25 @@ class FrontController extends Controller
         $categories = Category::all();
 
         $articles = ArticleNews::with(['category'])
-            ->where('is_featured', 'featured')
+            ->where('is_featured', 'not_featured')
             ->latest()
             ->take(3)
             ->get();
 
+        $featured_articles = ArticleNews::with(['category'])
+            ->where('is_featured', 'featured')
+            ->inRandomOrder()
+            ->take(3)
+            ->get();
+
+        $bannerads = BannerAdvertisement::where('is_active', 'active')
+            ->inRandomOrder()
+            ->where('type', 'banner')
+            // ->take(1)
+            ->first();
+
         $authors = Author::all();
-        
-        return view('front.index', compact('categories', 'articles', 'authors'));
+
+        return view('front.index', compact('categories', 'articles', 'featured_articles', 'authors', 'bannerads'));
     }
 }
