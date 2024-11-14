a5f77c58c0360f81243be29d0bfbf72d6bf56ac4
MaulidanFazri <maulidanfazri24@gmail.com>
2024-11-14 11:48:48 +0700

    Add category view and update FrontController for category-specific articles

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -81,4 +81,16 @@ class FrontController extends Controller
 
         return view('front.index', compact('categories', 'articles', 'featured_articles', 'authors', 'bannerads', 'entertainment_articles', 'entertainment_featured_articles', 'business_articles', 'business_featured_articles', 'automotive_articles', 'automotive_featured_articles'));
     }
+
+    public function category(Category $category) {
+        $categories = Category::all();
+
+        $bannerads = BannerAdvertisement::where('is_active', 'active')
+            ->inRandomOrder()
+            ->where('type', 'banner')
+            // ->take(1)
+            ->first();
+
+        return view('front.category', compact('category', 'categories', 'bannerads'));
+    }
 }
--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -23,7 +23,7 @@
             <form action="searchPage.html"
                 class="w-[450px] flex items-center rounded-full border border-[#E8EBF4] p-[12px_20px] gap-[10px] focus-within:ring-2 focus-within:ring-[#FF6B18] transition-all duration-300">
                 <button type="submit" class="w-5 h-5 flex shrink-0">
-                    <img src="assets/images/icons/search-normal.svg" alt="icon" />
+                    <img src="{{ asset('assets/images/icons/search-normal.svg') }}" alt="icon" />
                 </button>
                 <input type="text" name="" id=""
                     class="appearance-none outline-none w-full font-semibold placeholder:font-normal placeholder:text-[#A3A6AE]"
@@ -31,9 +31,6 @@
             </form>
         </div>
         <div class="flex items-center gap-3">
-            <a href=""
-                class="rounded-full p-[12px_22px] flex gap-[10px] font-semibold transition-all duration-300 border border-[#EEF0F7] hover:ring-2 hover:ring-[#FF6B18]">Upgrade
-                Pro</a>
             <a href=""
                 class="rounded-full p-[12px_22px] flex gap-[10px] font-bold transition-all duration-300 bg-[#FF6B18] text-white hover:shadow-[0_10px_20px_0_#FF6B1880]">
                 <div class="w-6 h-6 flex shrink-0">
@@ -169,7 +166,7 @@
             </a>
             <p class="font-medium text-sm leading-[21px] text-[#A3A6AE] flex gap-1">
                 Our Advertisement <a href="#" class="w-[18px] h-[18px]"><img
-                        src="assets/images/icons/message-question.svg" alt="icon" /></a>
+                        src="{{ asset('assets/images/icons/message-question.svg') }}" alt="icon" /></a>
             </p>
         </div>
     </section>
